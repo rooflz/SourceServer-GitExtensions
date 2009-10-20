3ef1ead503a90e0c9e2f9170e84ad8b54ca53ed6
Oliver, Jonathan <joliver@localhost>
2009-10-20 10:50:20 -0600

    Removed " (fetch)" value being appended to newer version of git for origin repository.
    Also encapsulated each value provided from the command line in quotes to ensure proper interpretation of commands being executed.

--- InSystemPath/gitcontents.bat
+++ InSystemPath/gitcontents.bat
@@ -6,22 +6,23 @@ if ()==(%3) exit /b
 if ()==(%4) exit /b
 
 setlocal
-set GIT_NODE_URL=%1
-set GIT_WORKING_DIR=%2
-set GIT_OBJ_ID=%3
-set GIT_CONTENTS_OUTPUT=%4
+set GIT_NODE_URL="%~1"
+set GIT_NODE_URL=%GIT_NODE_URL: (fetch)=%
+set GIT_WORKING_DIR="%~2"
+set GIT_OBJ_ID="%~3"
+set GIT_CONTENTS_OUTPUT="%~4"
 
 if exist %GIT_WORKING_DIR% goto update
 
 md %GIT_WORKING_DIR%
 git --git-dir=%GIT_WORKING_DIR% init
-git --git-dir=%GIT_WORKING_DIR% fetch "%GIT_NODE_URL%
+git --git-dir=%GIT_WORKING_DIR% fetch %GIT_NODE_URL%
 goto show
 
 :update
 for /f "usebackq delims=" %%i in (`git --no-pager --git-dir=%%GIT_WORKING_DIR%% show %%GIT_OBJ_ID%%`) do ( goto show )
 echo *Updating...*
-git --git-dir=%GIT_WORKING_DIR% fetch "%GIT_NODE_URL%
+git --git-dir=%GIT_WORKING_DIR% fetch %GIT_NODE_URL%
 
 :show
 if not exist %~dp4 mkdir %~dp4
